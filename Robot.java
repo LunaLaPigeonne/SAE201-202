e22c6273654c92ce96c5399e71b2cf5aa6c49dea
MacDeLuna <yann.gillot@uphf.fr>
2024-04-16 14:22:43 +0200

    Modification du fichier

--- Robot.java
+++ Robot.java
@@ -1,32 +1,3 @@
 public class Robot {
-      private String name;
-  private int x;
-  private int y;
 
-  public Robot(String name, int x, int y) {
-    this.name = name;
-    this.x = x;
-    this.y = y;
-  }
-
-  public void move(int dx, int dy) {
-    x += dx;
-    y += dy;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public int getX() {
-    return x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public String toString() {
-    return name + " is at (" + x + "," + y + ")";
-  }
 }
