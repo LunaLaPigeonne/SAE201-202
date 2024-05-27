7768b7d0bf2bf9a03d4229f45aae6cf32c4620a7
De-Clerck Rafael <Rafael.De-clerck@uphf.fr>
2024-05-27 20:52:50 +0200

    Restructuration des données

--- SAE 2.01/Code Source/Batiment.java
+++
@@ -1,8 +0,0 @@
-public abstract class Batiment extends Element {
-    public Batiment(int x, int y, int nbMaxMinerai, int nbMinerai, Minerais typeMinerais){
-        super(x, y, nbMaxMinerai,nbMinerai, typeMinerais);
-    }
-    public Batiment(int x, int y, int nbMaxMinerai, Minerais typeMinerais){
-        super(x, y, nbMaxMinerai, nbMaxMinerai, typeMinerais);
-    }
-}
